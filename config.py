09b427d081a2bc2514ddb80e579df6285e54b45c
Robert Gramlich <popestarkiller@icloud.com>
2021-09-18 09:08:19 -0700

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# Twitter token
-rds_connection_string = "postgres:postgres@nlp.csfqmbwx0bnr.us-west-1.rds.amazonaws.com:5432/NLP"
-
-token = 'AAAAAAAAAAAAAAAAAAAAACsyTgEAAAAAURHenAJyK1yFd4CLT%2BSsF3TgAk0%3DrbwRHkBkoL4xbX0b7I7PF0OjH4nkFJUXQiSfGiQa2WRjbPiabz'
